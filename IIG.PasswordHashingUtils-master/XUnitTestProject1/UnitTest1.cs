aa0967e1367d2262a12258200890f2a875a00457
StepanKobrii18321832 <esestepan2000@gmail.com>
2020-12-06 22:59:51 +0300

    update

--- IIG.PasswordHashingUtils-master/XUnitTestProject1/UnitTest1.cs
+++ IIG.PasswordHashingUtils-master/XUnitTestProject1/UnitTest1.cs
@@ -14,22 +14,22 @@ namespace XUnitTestProject1
         private static uint _modAdler32 = 1;
         private static uint _modAdler32_2 = 2;
         [Fact]
-        private void Test1()
+        private void TestNotNull()
         {
             Assert.NotNull(PasswordHasher.GetHash(_password));
         }
         [Fact]
-        private void Test2()
+        private void TestGetHash1()
         {
             Assert.Equal(PasswordHasher.GetHash(_password1), "E38CC12C4C3BDEC1A9689D0B2198DE1B93BFDB01333CC4EA76000B4730B854EE");
         }
         [Fact]
-        private void Test3()
+        private void TestGetHash2()
         {
             Assert.Equal(PasswordHasher.GetHash(_password2), "4D958688FF13DCC70543CFC8F93768BDE6280EACC6C84495DB9E5316D3656F50");
         }
         [Fact]
-        private void Test4()
+        private void TestInit()
         {
             PasswordHasher.Init(_salt, _modAdler32);
             Assert.Equal(PasswordHasher.GetHash(_password), "39F5A9915E7D0BF4514A6B0E23EFCC730996979430205EF1B17869DCD65B4C8E");
